623e293a59241066a5f0961b035ea9061a773fbd
zxm <chanceyzhang@qq.com>
2022-12-09 23:41:27 +0800

    Update:
    1. recover the original codes of repos parts.

--- include/FullSegmentInfo.h
+++ include/FullSegmentInfo.h
@@ -16,114 +16,114 @@ namespace upm {
  * The implementation is highly optimized to add or subtract new pixels and recompute all parameters.
  * Internally,
  */
-class FullSegmentInfo {
- public:
-  // Creates the FullSegmentInfo object but do not initialize internal structures
-  FullSegmentInfo(const std::vector<Pixel> &pts);
-  // Creates the FullSegmentInfo object and initializes internal structures
-  FullSegmentInfo(const std::vector<Pixel> &pts, int startIdx);
-  // Initialize the internal data structure from scratch
-  void init(const std::vector<Pixel> &pts, int startIdx);
-  // Checks if the point (x, y) is an inlier given the current line equation and a distance th.
-  inline bool isInlier(int x, int y, double lineFitErrThreshold) {
-    const double pointToLineDis = equation[0] * x + equation[1] * y + equation[2];
-    return UPM_ABS(pointToLineDis) < lineFitErrThreshold;
-  }
-
-  // Adds a new pixel (x, y) to the segment and update its parameters.
-  // pixelIndexInEdge is the index of the pixel in the edge structure.
-  // isPixelAtTheEnd True if the pixel is at the end of the segment or false if it is at the start
-  void addPixel(int x, int y, int pixelIndexInEdge, bool isPixelAtTheEnd = true);
-
-  // Finishes the detection of segment.
-  void finish();
-
-  void skipPositions();
-
-  inline double getFitError() const {
-    double dist, fitError = 0;
-    for (int i = firstPxIndex; i <= lastPxIndex; i++) {
-      dist = equation[0] * (*pixels)[i].x + equation[1] * (*pixels)[i].y + equation[2];
-      fitError += dist * dist;
+  class FullSegmentInfo {
+  public:
+    // Creates the FullSegmentInfo object but do not initialize internal structures
+    FullSegmentInfo(const std::vector<Pixel> &pts);
+    // Creates the FullSegmentInfo object and initializes internal structures
+    FullSegmentInfo(const std::vector<Pixel> &pts, int startIdx);
+    // Initialize the internal data structure from scratch
+    void init(const std::vector<Pixel> &pts, int startIdx);
+    // Checks if the point (x, y) is an inlier given the current line equation and a distance th.
+    inline bool isInlier(int x, int y, double lineFitErrThreshold) {
+      const double pointToLineDis = equation[0] * x + equation[1] * y + equation[2];
+      return UPM_ABS(pointToLineDis) < lineFitErrThreshold;
     }
-    return fitError / N;
-  }
 
-  void removeLastPx(bool removeFromTheEnd = true);
+    // Adds a new pixel (x, y) to the segment and update its parameters.
+    // pixelIndexInEdge is the index of the pixel in the edge structure.
+    // isPixelAtTheEnd True if the pixel is at the end of the segment or false if it is at the start
+    void addPixel(int x, int y, int pixelIndexInEdge, bool isPixelAtTheEnd = true);
 
-  inline void reset() {
-    sum_x_i = 0, sum_y_i = 0, sum_x_i_y_i = 0, sum_x_i_2 = 0, N = 0;
-    firstPxIndex = -1;
-    lastPxIndex = -1;
-    arePixelsSorted = true;
-    firstEndpointExtended = false;
-    secondEndpointExtended = false;
-  }
+    // Finishes the detection of segment.
+    void finish();
 
-  ///////////////////// Getters and setters /////////////////////
+    void skipPositions();
 
-  inline bool horizontal() const { return isHorizontal; }
+    inline double getFitError() const {
+      double dist, fitError = 0;
+      for (int i = firstPxIndex; i <= lastPxIndex; i++) {
+        dist = equation[0] * (*pixels)[i].x + equation[1] * (*pixels)[i].y + equation[2];
+        fitError += dist * dist;
+      }
+      return fitError / N;
+    }
 
-  inline int getNumOfPixels() const { return N; }
+    void removeLastPx(bool removeFromTheEnd = true);
 
-  inline const Segment &getEndpoints() const { return endpoints; }
+    inline void reset() {
+      sum_x_i = 0, sum_y_i = 0, sum_x_i_y_i = 0, sum_x_i_2 = 0, N = 0;
+      firstPxIndex = -1;
+      lastPxIndex = -1;
+      arePixelsSorted = true;
+      firstEndpointExtended = false;
+      secondEndpointExtended = false;
+    }
 
-  ImageEdge getPixels() const;
+    ///////////////////// Getters and setters /////////////////////
 
-  //Returns the Pixel in the first extreme of the segment
-  inline const Pixel &getFirstPixel() const { return firstPx; }
+    inline bool horizontal() const { return isHorizontal; }
 
-  //Returns the Pixel in the first extreme of the segment
-  inline const Pixel &getLastPixel() const { return lastPx; }
+    inline int getNumOfPixels() const { return N; }
 
-  // Returns a pointer to the first pixel of the segment
-  inline const Pixel *begin() const { return &((*pixels)[firstPxIndex]); }
+    inline const Segment &getEndpoints() const { return endpoints; }
 
-  // Returns a pointer to the position after the last pixel of the segment
-  inline const Pixel *end() const { return 1 + &((*pixels)[lastPxIndex]); }
+    ImageEdge getPixels() const;
 
-  inline const cv::Vec3d &getLineEquation() const { return equation; }
+    //Returns the Pixel in the first extreme of the segment
+    inline const Pixel &getFirstPixel() const { return firstPx; }
 
-  inline bool hasSecondSideElements() const { return !arePixelsSorted; }
+    //Returns the Pixel in the first extreme of the segment
+    inline const Pixel &getLastPixel() const { return lastPx; }
 
-  // True if the segment has been already extended in the direction of the first endpoint
-  bool firstEndpointExtended = false;
-  // Idem for the second one
-  bool secondEndpointExtended = false;
+    // Returns a pointer to the first pixel of the segment
+    inline const Pixel *begin() const { return &((*pixels)[firstPxIndex]); }
 
- private:
-  // Fits the line segment from scratch
-  void leastSquareLineFit(const std::vector<Pixel> &pts, int startIndex = 0);
-  // Adds a new pixel to the internal structures
-  void leastSquaresLineFitNewPoint(int x, int y);
-  // Subtracts a pixel from the internal structures
-  void subtractPointFromModel(const Pixel &p);
-  // Computes the line equation given the internal parameters (very efficient)
-  void calculateLineEq();
-  // Computes the segment endpoints given the internal parameters (very efficient)
-  void calcSegmentEndpoints();
+    // Returns a pointer to the position after the last pixel of the segment
+    inline const Pixel *end() const { return (&((*pixels)[lastPxIndex])) + 1; }
 
-  int64_t sum_x_i = 0, sum_y_i = 0, sum_x_i_y_i = 0, sum_x_i_2 = 0;
-  uint32_t N = 0;
-  int dx, dy;
-  // Tru if the line is mainly horizontal. When the line is closer to vertical,
-  // we invert the least squares and use y as independent coordinate and x as dependent coord.
-  bool isHorizontal;
-  // Pre-computed line segment endpoints
-  Segment endpoints;
-  // Line equation in format ax + by + c = 0
-  cv::Vec3d equation;
-  // Important pixels that are part of the segment
-  Pixel firstPx, prevFirstPx, lastPx, prevLastPx;
-  // Indices of some pixels in the chain
-  int firstPxIndex = -1, lastPxIndex = -1;
-  // Relevance of the segment
-  float salience = -1;
-  // Pointer to the chain of edge pixels
-  const ImageEdge *pixels;
-  // Indicates whenever the list of pixels from firstPxIndex to lastPxIndex is sorted
-  bool arePixelsSorted = true;
-};
+    inline const cv::Vec3d &getLineEquation() const { return equation; }
+
+    inline bool hasSecondSideElements() const { return !arePixelsSorted; }
+
+    // True if the segment has been already extended in the direction of the first endpoint
+    bool firstEndpointExtended = false;
+    // Idem for the second one
+    bool secondEndpointExtended = false;
+
+  private:
+    // Fits the line segment from scratch
+    void leastSquareLineFit(const std::vector<Pixel> &pts, int startIndex = 0);
+    // Adds a new pixel to the internal structures
+    void leastSquaresLineFitNewPoint(int x, int y);
+    // Subtracts a pixel from the internal structures
+    void subtractPointFromModel(const Pixel &p);
+    // Computes the line equation given the internal parameters (very efficient)
+    void calculateLineEq();
+    // Computes the segment endpoints given the internal parameters (very efficient)
+    void calcSegmentEndpoints();
+
+    int64_t sum_x_i = 0, sum_y_i = 0, sum_x_i_y_i = 0, sum_x_i_2 = 0;
+    uint32_t N = 0;
+    int dx, dy;
+    // Tru if the line is mainly horizontal. When the line is closer to vertical,
+    // we invert the least squares and use y as independent coordinate and x as dependent coord.
+    bool isHorizontal;
+    // Pre-computed line segment endpoints
+    Segment endpoints;
+    // Line equation in format ax + by + c = 0
+    cv::Vec3d equation;
+    // Important pixels that are part of the segment
+    Pixel firstPx, prevFirstPx, lastPx, prevLastPx;
+    // Indices of some pixels in the chain
+    int firstPxIndex = -1, lastPxIndex = -1;
+    // Relevance of the segment
+    float salience = -1;
+    // Pointer to the chain of edge pixels
+    const ImageEdge *pixels;
+    // Indicates whenever the list of pixels from firstPxIndex to lastPxIndex is sorted
+    bool arePixelsSorted = true;
+  };
 
 }
 
--- include/Utils.h
+++ include/Utils.h
@@ -1,9 +1,6 @@
 #ifndef ELSED_UTILS_H_
 #define ELSED_UTILS_H_
 
-
-#define _USE_MATH_DEFINES
-#include <cmath>
 #include <ostream>
 #include <opencv2/opencv.hpp>
 
@@ -29,79 +26,79 @@
 
 namespace upm {
 // Line segment in format [x0, y0, x1, y1] where the endpoints are (x0, y0) and (x1, y1)
-typedef cv::Vec4f Segment;
-typedef std::vector<Segment> Segments;
+  typedef cv::Vec4f Segment;
+  typedef std::vector<Segment> Segments;
 
 /**
  * Data structure containing the gradient information of an image
  */
-struct LineDetectionExtraInfo {
-  cv::Mat dxImg;   // Store the dxImg (horizontal gradient)
-  cv::Mat dyImg;   // Store the dyImg (vertical gradient)
-  cv::Mat gImgWO;  // Store the gradient image without threshold
-  cv::Mat gImg;    // Store the gradient image
-  cv::Mat dirImg;  // Store the direction image
+  struct LineDetectionExtraInfo {
+    cv::Mat dxImg;   // Store the dxImg (horizontal gradient)
+    cv::Mat dyImg;   // Store the dyImg (vertical gradient)
+    cv::Mat gImgWO;  // Store the gradient image without threshold
+    cv::Mat gImg;    // Store the gradient image
+    cv::Mat dirImg;  // Store the direction image
 
-  // image sizes
-  unsigned int imageWidth;
-  unsigned int imageHeight;
-};
+    // image sizes
+    unsigned int imageWidth;
+    unsigned int imageHeight;
+  };
 // Pointer to the struct
-typedef std::shared_ptr<LineDetectionExtraInfo> LineDetectionExtraInfoPtr;
+  typedef std::shared_ptr<LineDetectionExtraInfo> LineDetectionExtraInfoPtr;
 
 /**
  * Simple data structure representing a pixel. Unlike cv::Point2i this class is
  * trivial (trivially copyable and has a trivial default constructor).
  */
-struct Pixel {
-  int x;
-  int y;
+  struct Pixel {
+    int x;
+    int y;
 
-  Pixel() = default;
-  Pixel(int x, int y) : x(x), y(y) {}
-  explicit Pixel(const cv::Point &p) : x(p.x), y(p.y) {}
+    Pixel() = default;
+    Pixel(int x, int y) : x(x), y(y) {}
+    explicit Pixel(const cv::Point &p) : x(p.x), y(p.y) {}
 
-  inline bool operator==(const Pixel &rhs) const {
-    return x == rhs.x && y == rhs.y;
-  }
-  inline bool operator!=(const Pixel &rhs) const {
-    return !(rhs == *this);
-  }
+    inline bool operator==(const Pixel &rhs) const {
+      return x == rhs.x && y == rhs.y;
+    }
+    inline bool operator!=(const Pixel &rhs) const {
+      return !(rhs == *this);
+    }
 
-  inline bool operator<(const Pixel &px2) const {
-    // Compare the pixels by its squared L1 distance to the origin
-    return (x + y) < (px2.x + px2.y);
-  }
-};
+    inline bool operator<(const Pixel &px2) const {
+      // Compare the pixels by its squared L1 distance to the origin
+      return (x + y) < (px2.x + px2.y);
+    }
+  };
 
 // Define the edge types
-typedef std::vector<Pixel> ImageEdge;
-typedef std::vector<ImageEdge> ImageEdges;
+  typedef std::vector<Pixel> ImageEdge;
+  typedef std::vector<ImageEdge> ImageEdges;
 
 /**
  * This struct represents a segment weighted by its importance in the image.
  */
-struct SalientSegment {
-  Segment segment;
-  double salience;
+  struct SalientSegment {
+    Segment segment;
+    double salience;
 
-  SalientSegment() = default;
-  SalientSegment(const Segment &segment, double salience) : segment(segment), salience(salience) {}
+    SalientSegment() = default;
+    SalientSegment(const Segment &segment, double salience) : segment(segment), salience(salience) {}
 
-  inline bool operator<(const SalientSegment &rhs) const {
-    if (salience == rhs.salience) {
-      float dx1 = segment[0] - segment[2];
-      float dx2 = rhs.segment[0] - rhs.segment[2];
-      float dy1 = segment[1] - segment[3];
-      float dy2 = rhs.segment[1] - rhs.segment[3];
-      return std::sqrt(dx1 * dx1 + dy1 * dy1) > std::sqrt(dx2 * dx2 + dy2 * dy2);
-    } else {
-      return salience > rhs.salience;
+    inline bool operator<(const SalientSegment &rhs) const {
+      if (salience == rhs.salience) {
+        float dx1 = segment[0] - segment[2];
+        float dx2 = rhs.segment[0] - rhs.segment[2];
+        float dy1 = segment[1] - segment[3];
+        float dy2 = rhs.segment[1] - rhs.segment[3];
+        return std::sqrt(dx1 * dx1 + dy1 * dy1) > std::sqrt(dx2 * dx2 + dy2 * dy2);
+      } else {
+        return salience > rhs.salience;
+      }
     }
-  }
-};
+  };
 
-typedef std::vector<SalientSegment> SalientSegments;
+  typedef std::vector<SalientSegment> SalientSegments;
 
 /**
  * @brief Calculate the angular distance between two angles valueA and valueB.
@@ -111,22 +108,22 @@ typedef std::vector<SalientSegment> SalientSegments;
  * @param mod The number of elements in the circle (2 * PI to radians or 360 for degrees)
  * @return
  */
-inline double
-circularDist(double valueA, double valueB, double mod = 360) {
-  double a, b; // a is the small number and b the great number
-  if (valueA < valueB) {
-    a = valueA;
-    b = valueB;
-  } else {
-    a = valueB;
-    b = valueA;
-  }
+  inline double
+  circularDist(double valueA, double valueB, double mod = 360) {
+    double a, b; // a is the small number and b the great number
+    if (valueA < valueB) {
+      a = valueA;
+      b = valueB;
+    } else {
+      a = valueB;
+      b = valueA;
+    }
 
-  double dist_clockwise, dist_no_clockwise;
-  dist_clockwise = b - a;
-  dist_no_clockwise = a + (mod - b);
-  return std::min(dist_clockwise, dist_no_clockwise);
-}
+    double dist_clockwise, dist_no_clockwise;
+    dist_clockwise = b - a;
+    dist_no_clockwise = a + (mod - b);
+    return std::min(dist_clockwise, dist_no_clockwise);
+  }
 
 /**
  * @brief Gets the projection of a point into a line.
@@ -134,36 +131,36 @@ circularDist(double valueA, double valueB, double mod = 360) {
  * @param p The point which we want to calculate its projection over the line
  * @return The point p' which is the projection of p over the line.
  */
-inline cv::Point2f
-getProjectionPtn(const cv::Vec3f &l, const cv::Point2f &p) {
-  const cv::Vec3f homoP(p.x, p.y, 1);
-  if (l.dot(homoP) == 0) {
-    // If the point is over the line return this same point
-    return p;
+  inline cv::Point2f
+  getProjectionPtn(const cv::Vec3f &l, const cv::Point2f &p) {
+    const cv::Vec3f homoP(p.x, p.y, 1);
+    if (l.dot(homoP) == 0) {
+      // If the point is over the line return this same point
+      return p;
+    }
+    // Since the direction of l is (-l.b, l.a), the rotated 90 degrees vector will be: (l.a, l.b)
+    // The direction vector of the perpendicular rect we want to calc.
+    const cv::Vec2f v2(l[0], l[1]);
+    // Rect with direction v2 passing by point p
+    const cv::Vec3f r2(v2[1], -v2[0], v2[0] * p.y - v2[1] * p.x);
+    // return the intersection between the two lines in cartesian coordinates
+    const cv::Vec3f p2 = l.cross(r2);
+    return cv::Point2f(p2[0] / p2[2], p2[1] / p2[2]);
   }
-  // Since the direction of l is (-l.b, l.a), the rotated 90 degrees vector will be: (l.a, l.b)
-  // The direction vector of the perpendicular rect we want to calc.
-  const cv::Vec2f v2(l[0], l[1]);
-  // Rect with direction v2 passing by point p
-  const cv::Vec3f r2(v2[1], -v2[0], v2[0] * p.y - v2[1] * p.x);
-  // return the intersection between the two lines in cartesian coordinates
-  const cv::Vec3f p2 = l.cross(r2);
-  return cv::Point2f(p2[0] / p2[2], p2[1] / p2[2]);
-}
 
 /**
  * @brief Calculates the length of a line segment
  * @param s The input segment
  * @return The length of the segment
  */
-inline float
-segLength(const Segment &s) {
-  // The optimal way to do that, is to compute first the differences
-  const float dx = s[0] - s[2];
-  const float dy = s[1] - s[3];
-  // And after that, do the square root, avoiding the use of double
-  return std::sqrt(dx * dx + dy * dy);
-}
+  inline float
+  segLength(const Segment &s) {
+    // The optimal way to do that, is to compute first the differences
+    const float dx = s[0] - s[2];
+    const float dy = s[1] - s[3];
+    // And after that, do the square root, avoiding the use of double
+    return std::sqrt(dx * dx + dy * dy);
+  }
 
 /**
  * @brief Calculates the angle of a line segment
@@ -171,13 +168,13 @@ segLength(const Segment &s) {
  * @return The angle that the segment forms with the X-axis in radians.
  * Range (pi/2, -pi/2].
  */
-inline float
-segAngle(const Segment &s) {
-  if (s[2] > s[0])
-    return std::atan2(s[3] - s[1], s[2] - s[0]);
-  else
-    return std::atan2(s[1] - s[3], s[0] - s[2]);
-}
+  inline float
+  segAngle(const Segment &s) {
+    if (s[2] > s[0])
+      return std::atan2(s[3] - s[1], s[2] - s[0]);
+    else
+      return std::atan2(s[1] - s[3], s[0] - s[2]);
+  }
 
 
 /**
@@ -189,54 +186,54 @@ segAngle(const Segment &s) {
  * @param y1 The Y coordinate of the last endpoint
  * @return
  */
-static std::vector<Pixel> bresenham(int x0, int y0, int x1, int y1) {
-  int dx, dy, p, x, y, xIncrement, yIncrement;
-  std::vector<Pixel> pixels;
-  dx = x1 - x0;
-  dy = y1 - y0;
+  static std::vector<Pixel> bresenham(int x0, int y0, int x1, int y1) {
+    int dx, dy, p, x, y, xIncrement, yIncrement;
+    std::vector<Pixel> pixels;
+    dx = x1 - x0;
+    dy = y1 - y0;
 
-  // Determine the line direction
-  xIncrement = dx < 0 ? -1 : +1;
-  yIncrement = dy < 0 ? -1 : +1;
+    // Determine the line direction
+    xIncrement = dx < 0 ? -1 : +1;
+    yIncrement = dy < 0 ? -1 : +1;
 
-  x = x0;
-  y = y0;
-  dx = UPM_ABS(dx);
-  dy = UPM_ABS(dy);
-  // pixels.reserve(std::max(dx, dy));
+    x = x0;
+    y = y0;
+    dx = UPM_ABS(dx);
+    dy = UPM_ABS(dy);
+    // pixels.reserve(std::max(dx, dy));
 
-  if (dx >= dy) {
-    // Horizontal like line
-    p = 2 * dy - dx;
-    while (x != x1) {
-      pixels.emplace_back(x, y);
-      if (p >= 0) {
-        y += yIncrement;
-        p += 2 * dy - 2 * dx;
-      } else {
-        p += 2 * dy;
-      }
-      // Increment the axis in which we are moving
-      x += xIncrement;
-    }  // End of while
-  } else {
-    // Vertical like line
-    p = 2 * dx - dy;
-    while (y != y1) {
-      pixels.emplace_back(x, y);
-      if (p >= 0) {
+    if (dx >= dy) {
+      // Horizontal like line
+      p = 2 * dy - dx;
+      while (x != x1) {
+        pixels.emplace_back(x, y);
+        if (p >= 0) {
+          y += yIncrement;
+          p += 2 * dy - 2 * dx;
+        } else {
+          p += 2 * dy;
+        }
+        // Increment the axis in which we are moving
         x += xIncrement;
-        p += +2 * dx - 2 * dy;
-      } else {
-        p += 2 * dx;
-      }
-      // Increment the axis in which we are moving
-      y += yIncrement;
-    }  // End of while
+      }  // End of while
+    } else {
+      // Vertical like line
+      p = 2 * dx - dy;
+      while (y != y1) {
+        pixels.emplace_back(x, y);
+        if (p >= 0) {
+          x += xIncrement;
+          p += +2 * dx - 2 * dy;
+        } else {
+          p += 2 * dx;
+        }
+        // Increment the axis in which we are moving
+        y += yIncrement;
+      }  // End of while
+    }
+    pixels.emplace_back(x1, y1);
+    return pixels;
   }
-  pixels.emplace_back(x1, y1);
-  return pixels;
-}
 }  // namespace upm
 
 #endif  // ELSED_UTILS_H_
